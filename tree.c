0ce7b84791d8fd86e7b0fb784198a269055e8faf
zakjamal123 <44930449+zakjamal123@users.noreply.github.com>
2021-03-11 18:06:13 -0600

    Base code for Tree
    Added the orders and the base setup for the tree

--- tree.c
+++ tree.c
@@ -1,15 +1,78 @@
-t_node* parseExpression(string e){
-	 
-	 
-	
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode
+{
+	int elem;
+	struct TreeNode *left;
+	struct TreeNode *right;
+} node;
+
+node *newNode(int val)
+{
+
+	node *ptr = (node *)malloc(sizeof(node));
+	ptr->elem = val;
+	ptr->left = NULL;
+	ptr->right = NULL;
+	return ptr;
 }
 
-t_node* newNode(node* stack_node){
-	
-	 t_node* leafNode = (t_node*) malloc(sizeof(t_node));
-	 leafNode->elem = *stack_node;
-	 leafNode->left = NULL;
-	 leafNode->right = NULL;
-	 
-	 return leafNode;	
+void preOrderprint(node *r)
+{
+	if (r == NULL)
+		return;
+	//print node data
+	printf("%d,  ", r->elem);
+
+	// visit left subtree
+	preOrderprint(r->left);
+	//visit right subtree
+	preOrderprint(r->right);
+}
+
+void inOrderprint(node *r)
+{
+	if (r == NULL)
+		return;
+	// visit left subtree
+	inOrderprint(r->left);
+	//print node data
+	printf("%d,  ", r->elem);
+
+	//visit right subtree
+	inOrderprint(r->right);
+}
+
+void postOrderprint(node *r)
+{
+	if (r == NULL)
+		return;
+	// visit left subtree
+	postOrderprint(r->left);
+
+	//visit right subtree
+	postOrderprint(r->right);
+	//print node data
+	printf("%d,  ", r->elem);
+}
+
+int main(int argc, char const *argv[])
+{
+
+	node *root = newNode(6);
+	root->left = newNode(8);
+	root->right = newNode(3);
+	root->left->left = newNode(9);
+	root->right->left = newNode(1);
+	root->right->right = newNode(5);
+
+	preOrderprint(root);
+	printf("\n");
+	inOrderprint(root);
+	printf("\n");
+	postOrderprint(root);
+	printf("\n");
+
+	return 0;
 }
